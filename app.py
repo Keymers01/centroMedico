a6a2f1be37003549218fa601e794de4e4f5159bc
Carlos Iriarte <car.iriarte@duocuc.cl>
2022-05-11 22:46:16 -0400

    Integracion de diseño

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from notifypy import Notify
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'matador1711'
+app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'app_medicenter'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
